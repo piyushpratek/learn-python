450b51619f55d6d6d5db489bf3c84f64e579f6e2
Piyush Prateek <piyushprateek231996@gmail.com>
2025-01-20 22:44:37 +0530

    commit 2

--- 1.py
+++ 1.py
@@ -20,3 +20,5 @@ print(type({"name": "Asabeneh"}))  #            <class 'dict'>
 print(type({9.8, 3.14, 2.7}))  #                <class 'set'>
 print(type((9.8, 3.14, 2.7)))  #                <class 'tuple'>
 print()
+
+# 123
